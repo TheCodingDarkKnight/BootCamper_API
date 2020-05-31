341206078c73f8ff418700a5558522312286aafb
Rahul Pote <58665295+TheCodingDarkKnight@users.noreply.github.com>
2020-05-31 13:54:50 +0530

    Custom ErrorHandler: Duplucate value

--- middleware/error.js
+++ middleware/error.js
@@ -3,13 +3,20 @@ const ErrorResponse = require("../utils/errorResponse");
 const errorHandler = (err, req, res, next) => {
   let error = { ...err };
   // Log to console for Dev
-  console.log("error stack", err.stack.red);
+  console.log("error stack", err.stack);
 
   // console.log("error name", error.name);
+
   // Mongoose Bad ObjectId
   if (error.name === "CastError") {
     const message = `Resource not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose Duplicate key
+  if (error.code === 11000) {
+    const message = `Duplicate field value entered`;
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
