5ae102c9cd98c5f17e1f166392d99d298ebf0a12
Rahul Pote <58665295+TheCodingDarkKnight@users.noreply.github.com>
2020-06-10 21:48:49 +0530

    User: add JWT token for auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,8 +16,12 @@ exports.register = asyncHandler(async (req, res, next) => {
     password,
     role,
   });
+
+  // Create token
+  const token = user.getSignedJwtToken();
   res.status(200).json({
     success: true,
+    token,
     data: user,
   });
 });
