95a8b7fec608c0ddd5fbbd77bf11cdd1823ce7ab
Rahul Pote <58665295+TheCodingDarkKnight@users.noreply.github.com>
2020-06-13 23:40:28 +0530

    Add user seeder

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: "./config/config.env" });
 // Load Models
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
+const User = require("./models/User");
 
 // Connect to DB
 
@@ -27,14 +28,20 @@ const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
 );
 
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
+);
+
 // Import into DB
 const importData = async () => {
   try {
     const bootcamp = await Bootcamp.create(bootcamps);
     const course = await Course.create(courses);
+    const user = await User.create(users);
 
     console.log(`${bootcamp.length} Bootcamps Imported...`.green.inverse);
     console.log(`${course.length} Courses Imported...`.green.inverse);
+    console.log(`${user.length} Users Imported...`.green.inverse);
 
     process.exit();
   } catch (err) {
@@ -47,6 +54,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
 
     console.log(`Data Destroyed...`.red.inverse);
     process.exit();
