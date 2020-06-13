182d284167f38fefb3a9911515de5542d06fff22
Rahul Pote <58665295+TheCodingDarkKnight@users.noreply.github.com>
2020-06-13 22:08:06 +0530

    Add Publisher only add one bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -15,6 +15,22 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 //@route    GET /api/v1/bootcamps
 //@access   Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  // Add user to req.body
+  req.body.user = req.user.id;
+
+  // Checks for published bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+  // IF the user is not an admin, they can only add one bootcamp
+  if (publishedBootcamp && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `The user with id ${req.user.id} has already published a bootcamp`,
+        400
+      )
+    );
+  }
+
   const data = await Bootcamp.create(req.body);
   res.status(201).json({ success: true, data });
 });
