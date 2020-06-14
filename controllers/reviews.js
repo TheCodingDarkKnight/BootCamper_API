2ec231ee3ce891f96fbb6fd72f255d42e2b31181
Rahul Pote <58665295+TheCodingDarkKnight@users.noreply.github.com>
2020-06-14 23:51:22 +0530

    Review: delete

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -88,3 +88,28 @@ exports.updateReview = asyncHandler(async (req, res, next) => {
 
   res.status(200).json({ success: true, data: review });
 });
+
+//@desc     Delete review
+//@route    DELETE /api/v1/reviews/:id
+//@access   Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+  if (!review) {
+    return next(
+      new ErrorResponse(`Review not found with id ${req.params.id}`, 400)
+    );
+  }
+
+  // Make sure review belongs to user or user is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.params.id} is not authorized to delete the review`,
+        401
+      )
+    );
+  }
+
+  review.remove();
+  res.status(200).json({ success: true, data: {} });
+});
