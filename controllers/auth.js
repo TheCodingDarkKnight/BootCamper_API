7e20ffd76a1a8bb3320fed0985d7a4f545c367d9
Rahul Pote <58665295+TheCodingDarkKnight@users.noreply.github.com>
2020-06-14 20:05:34 +0530

    User: update user details
    Update email address and name

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,7 @@ const asyncHandler = require("../middleware/async");
 const sendEmail = require("../utils/sendEmail");
 
 const User = require("../models/User");
+const { truncate } = require("fs");
 
 //@desc     Register user
 //@route    POST /api/v1/auth/register
@@ -54,6 +55,25 @@ exports.login = asyncHandler(async (req, res, next) => {
 //@access   Private
 exports.getMe = asyncHandler(async (req, res, next) => {
   const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+//@desc     Update user details
+//@route    PUT /api/v1/auth/updatedetails
+//@access   Private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true,
+  });
 
   res.status(200).json({
     success: true,
