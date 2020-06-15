1c0e0196c257bf2f7810636310da3eff0a984baf
Rahul Pote <58665295+TheCodingDarkKnight@users.noreply.github.com>
2020-06-15 20:50:11 +0530

    Prevent NoSQL Injection & Sanitize Data

--- middleware/error.js
+++ middleware/error.js
@@ -12,7 +12,7 @@ const errorHandler = (err, req, res, next) => {
 
   // Mongoose Bad ObjectId
   if (error.name === "CastError") {
-    const message = `Resource not found with id of ${err.value}`;
+    const message = `Resource not found`;
     error = new ErrorResponse(message, 404);
   }
 
